bc01d2787158ba01a5c9f40f8efcd3471cfaf26a
Joshua <joshamyjarami@hotmail.com>
2023-02-04 22:25:17 -0700

    planet swapping with a killer planet

--- Assets/Player/planet/PlanetSpawn.cs
+++ Assets/Player/planet/PlanetSpawn.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
 
 public class PlanetSpawn : MonoBehaviour
 {
@@ -10,14 +11,16 @@ public class PlanetSpawn : MonoBehaviour
     [SerializeField] Sprite planet3;
     [SerializeField] Sprite planet4;
 
+    public float planetSize;
+
     int randomNum = 0;
     // Start is called before the first frame update
     void Start()
     {
         randomNum = Random.Range(0, 3);
-        if(randomNum > 2)
+        if (randomNum > 2)
         {
-            
+
         }
         else
         {
@@ -25,11 +28,11 @@ public class PlanetSpawn : MonoBehaviour
         }
         Sprite[] planetSprites = new Sprite[] { planet1, planet2, planet3, planet4 };
         GetComponent<SpriteRenderer>().sprite = planetSprites[randomNum];
-    }
+        transform.localScale = new Vector2(planetSize, planetSize);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+        }
     }
 }
